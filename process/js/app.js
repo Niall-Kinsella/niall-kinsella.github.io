dd68b5c1f0d7bf099a212d6d1429fbdb3bb6858e
Niall-Kinsella <kinsella753@gmail.com>
2017-07-07 18:45:19 +0100

    commit pets app

--- process/js/app.js
+++ process/js/app.js
@@ -88,3 +88,37 @@ $(function() {
   });
 
 });
+
+
+// Check network status toast
+(function () {
+  'use strict';
+
+  var header = document.querySelector('header');
+  var menuHeader = document.querySelector('.menu__header');
+
+  //After DOM Loaded
+  document.addEventListener('DOMContentLoaded', function(event) {
+    //On initial load to check connectivity
+    if (!navigator.onLine) {
+      updateNetworkStatus();
+    }
+
+    window.addEventListener('online', updateNetworkStatus, false);
+    window.addEventListener('offline', updateNetworkStatus, false);
+  });
+
+  //To update network status
+  function updateNetworkStatus() {
+    if (navigator.onLine) {
+      toast('You are online..');
+      header.classList.remove('app__offline');
+      menuHeader.style.background = '#1E88E5'; 
+    }
+    else {
+      toast('You are now offline..');
+      header.classList.add('app__offline');
+      menuHeader.style.background = '#9E9E9E';
+    }
+  }
+})();
